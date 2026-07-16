849044349f5d7dd8da03cfebfa6511c0d167c272
Leonardo Muniz <131314978+leonardo-muniz@users.noreply.github.com>
2026-07-15 22:19:21 -0300

    test: add initial test class for ExchangeApi application context

---
+++ exchange-api/src/test/java/com/exchange/crypto/ExchangeApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.exchange.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ExchangeApiApplicationTests {
+	@Test
+	void contextLoads() {}
+}
